2a09a675ac03f677360ac2d41c2f8256a9d8c0c2
VedantFNO <123ved@gmail.com>
2018-09-05 14:13:56 -0500

    Skeletal setup for all three environments, random policy for GridWorld, and report Jupyter notebook.

--- homework/hw1/hw1_vedant2/gridWorld/gridworld.py
+++ homework/hw1/hw1_vedant2/gridWorld/gridworld.py
@@ -17,10 +17,10 @@ South = 1
 East = 2
 North = 3
 
-MAP = {["EAEBE",
+MAP = ["EAEBE",
         "ESSSE",
         "ESSSE",
-        "ESSSE"]}
+        "ESSSE"]
 
 class GirdWorldEnv(discrete.DiscreteEnv):
     """
@@ -33,60 +33,67 @@ special states A and B. From state A, all four actions yield a reward of +10 and
 agent to A0. From state B, all actions yield a reward of +5 and take the agent to B0.
     """
 metadata = {'render.modes': ['human', 'ansi']}
-    def __init__(self):
-        self.nrow, self.ncol = 5, 5
+    def __init__(self, hard=False):
+        
+        self.nrow, self.ncol = 5, 5;
 
-        nA = 4
-        nS = nrow * ncol
-
+        nA = 4;
+        nS = nrow * ncol;
+        desc = np.asarray(MAP,dtype='c');
+        isd = np.array(desc == b'S').astype('float64').ravel();
+        isd /= isd.sum();
+        
         P = {s : {a : [] for a in range(nA)} for s in range(nS)}
 
         def to_s(row, col):
-            return row*ncol + col
+            return row*ncol + col;
         
         def inc(row, col, a):
-		reward = 0;
-		#check if in A or B
-		if (row == 0 && col == 1 ):
-			reward = 10;
-			row = nrow-1;
-		elif (row == 0 && col == 3 ):
-			reward = 5;
-			row = 2;
-		else:
-            if a==0: # West
-				col = col-1;
-				if (col<0):
-					col = 0;
-					reward = -1;
-            elif a==1: # South
-                row = row+1
-				if (row>nrow-1);
-					row = nrow-1;
-					reward = -1;
-            elif a==2: # East
-                col = col+1;
-				if(col>ncol-1):
-					col = ncol-1;
-					reward = -1;
-            elif a==3: # North
-                row = row-1;
-				if (row<0):
-					row = 0;
-					reward = -1;
-            return (row, col,reward)
+            probability = 1.0/4.0;
+            reward = 0;
+            #check if in A or B
+            if (row == 0 && col == 1 ):
+                reward = 10;
+                row = nrow-1;
+                probability = 1.0;
+		    elif (row == 0 && col == 3 ):
+			    reward = 5;
+			    row = 2;
+                probability = 1.0;
+		    else:
+                if a==0: # West
+				    col = col-1;
+				    if (col<0):
+				   	    col = 0;
+					    reward = -1;
+                elif a==1: # South
+                    row = row+1
+				    if (row>nrow-1);
+				    	row = nrow-1;
+					    reward = -1;
+                elif a==2: # East
+                    col = col+1;
+				    if(col>ncol-1):
+					    col = ncol-1;
+					    reward = -1;
+                elif a==3: # North
+                    row = row-1;
+				    if (row<0):
+					    row = 0;
+					    reward = -1;
+            return (row, col,reward, probability)
 
         for row in range(nrow):
             for col in range(ncol):
                 s = to_s(row, col)
                 for a in range(4):
                     li = P[s][a]
-		    newrow, newcol = inc(row, col, a)
-		    newstate = to_s(newrow, newcol)
-		    li.append((1.0, newstate, rew, done))
-        super(GirdWorldEnv, self).__init__(nS, nA, P)
+		            newrow, newcol, rew, probability = inc(row, col, a)
+		            newstate = to_s(newrow, newcol)
+		            li.append((probability, newstate, rew, False))
+        super(GirdWorldEnv, self).__init__(nS, nA, P, isd)
 
-    def render(self, mode='human'):
+     def render(self, mode='human'):
         outfile = StringIO() if mode == 'ansi' else sys.stdout
 
         row, col = self.s // self.ncol, self.s % self.ncol
@@ -94,7 +101,7 @@ metadata = {'render.modes': ['human', 'ansi']}
         desc = [[c.decode('utf-8') for c in line] for line in desc]
         desc[row][col] = utils.colorize(desc[row][col], "red", highlight=True)
         if self.lastaction is not None:
-            outfile.write("  ({})\n".format(["West","South","East","North"][self.lastaction]))
+            outfile.write("  ({})\n".format(["Left","Down","Right","Up"][self.lastaction]))
         else:
             outfile.write("\n")
         outfile.write("\n".join(''.join(line) for line in desc)+"\n")
