2b62aa00765fa110e9a1f00427989b643b0b65eb
akshayshetty <ashetty2@illinois.edu>
2018-09-05 14:24:09 -0500

    HW1 submission

--- homework/hw1/hw1_ashetty2/gridworld/gridworld.py
+++ homework/hw1/hw1_ashetty2/gridworld/gridworld.py
@@ -1,18 +1,86 @@
 import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
+import numpy as np
+
+GRIDMAP = ["-A-B-",
+           "-----",
+           "---b-",
+           "-----",
+           "-a---"]
 
 class GridWorld(gym.Env):
-  metadata = {'render.modes': ['human']}
+	metadata = {'render.modes': ['human']}
 
-  def __init__(self):
-    print('GridWorld loaded')
+	def __init__(self):
+		#print('GridWorld loaded')
+		self.state = None
 
-  def step(self, action):
-    print('A')
+	def step(self, action):
+		rown = self.state[0]
+		coln = self.state[1]
+		done = False
+		info = {}
+		
+		#if at A
+		if rown==0 and coln==1:
+			new_row = 4
+			new_col = 1
+			reward = 10
+			self.state = np.array([new_row, new_col])
+			return self.state, reward, done, info
+		#if at B
+		elif rown==0 and coln==3:
+			new_row = 2
+			new_col = 3
+			reward = 5
+			self.state = np.array([new_row, new_col])
+			return self.state, reward, done, info
 
-  def reset(self):
-    print('A')
+		new_row, new_col = rown, coln
+		reward = 0
 
-  def render(self, mode='human', close=False):
-    print('A')
+		if action=="N":
+			if rown==0:
+				reward = -1
+			else:
+				new_row -= 1
+		elif action=="S":
+			if rown==4:
+				reward = -1
+			else:
+				new_row += 1
+		elif action=="E":
+			if coln==4:
+				reward = -1
+			else:
+				new_col += 1
+		elif action=="W":
+			if coln==0:
+				reward = -1
+			else:
+				new_col -= 1
+
+		self.state = np.array([new_row, new_col])
+		return self.state, reward, done, info
+
+
+	def reset(self):
+		self.state = np.random.randint(5, size=2)
+		#print('Generate random initial state:', self.state)	
+		return self.state
+
+	def render(self, mode='human', close=False):
+		rown = self.state[0]
+		coln = self.state[1]
+		for row in range(0,5):
+			if row == rown:
+				temp_str = GRIDMAP[row][0:coln] + "X" + GRIDMAP[row][coln+1:]
+			else:
+				temp_str = GRIDMAP[row]
+			print(temp_str)
+
+	def action_space(self):
+		action = np.random.randint(4)
+		action_str = "NSEW"
+		return action_str[action]
