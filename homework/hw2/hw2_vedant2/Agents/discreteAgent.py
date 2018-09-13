f28498599059feb0b717c0690180b7489a077e4b
VedantFNO <123ved@gmail.com>
2018-09-13 00:46:22 -0500

    SARSA and Q learning work for both cases. Final submission

---
+++ homework/hw2/hw2_vedant2/Agents/discreteAgent.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 12 22:03:38 2018
+
+@author: Vedant
+"""
+
+import time, pickle, os
+import numpy as np
+
+def SARSA(env,initial_epsilon = 1, final_epsilon = 0.01,total_episodes = 1000, annealing_period = None,max_steps = 25,lr_rate = 0.9,gamma = 0.9, decay_rate = None):
+    if (annealing_period == None):
+        annealing_period = total_episodes;
+    if(annealing_period > total_episodes):
+        print('Annealing period cannot be more than training period, setting annealing period equal to training period')
+        annealing_period = total_episodes;
+    Q = np.zeros((env.observation_space.n, env.action_space.n))
+    epsilon = initial_epsilon;
+    def choose_action(state):
+        action=0
+        if np.random.uniform(0, 1) < epsilon:
+            action = env.action_space.sample()
+        else:
+            action = np.argmax(Q[state, :])
+        return action
+
+    def learn(state, state2, reward, action, action2):
+        predict = Q[state, action]
+        target = reward + gamma * Q[state2, action2]
+        Q[state, action] = predict + lr_rate * (target - predict)
+    # Start
+    erewards=[]
+    for episode in range(total_episodes):
+        crewards=0
+        t = 0
+        state = env.reset()
+        action = choose_action(state)
+        while t < max_steps:
+            #env.render('human')
+            state2, reward, done, info = env.step(action)
+            action2 = choose_action(state2)
+            learn(state, state2, reward, action, action2)
+            state = state2
+            action = action2
+            t += 1
+            crewards+=reward
+            if done:
+                print(episode)
+                break
+        if decay_rate != None:
+            epsilon = initial_epsilon + (final_epsilon - initial_epsilon) * np.exp(-decay_rate * episode) 
+        epsilon = initial_epsilon + (final_epsilon - initial_epsilon) * (episode+1)/total_episodes
+        erewards.append(crewards);
+        
+      # os.system('clear')
+            #time.sleep(0.1)
+    #print ("Score over time: ", rewards/total_episodes)
+    #print ("Score in last trial: ", erewards)
+    erewards = np.array(erewards)
+    return (Q, erewards)
+    #with open("frozenLake_qTable_sarsa.pkl", 'wb') as f:
+        #pickle.dump(Q, f)
+        
+def QLearn(env,initial_epsilon = 1, final_epsilon = 0.01,total_episodes = 1000, annealing_period = None,max_steps = 25,lr_rate = 0.9,gamma = 0.9, decay_rate = None):
+    if (annealing_period == None):
+        annealing_period = total_episodes;
+    if(annealing_period > total_episodes):
+        print('Annealing period cannot be more than training period, setting annealing period equal to training period')
+        annealing_period = total_episodes;
+    Q = np.zeros((env.observation_space.n, env.action_space.n))
+    epsilon = initial_epsilon;
+    def choose_action(state):
+        action=0
+        if np.random.uniform(0, 1) < epsilon:
+            action = env.action_space.sample()
+        else:
+            action = np.argmax(Q[state])
+        return action
+
+    def learn(state, state2, reward, action):
+        predict = Q[state, action]
+        target = reward + gamma * Q[state2, np.argmax(Q[state2])]
+        Q[state, action] = predict + lr_rate * (target - predict)
+    # Start
+    erewards=[]
+    for episode in range(total_episodes):
+        crewards=0
+        t = 0
+        state = env.reset()
+       
+        while t < max_steps:
+            action = choose_action(state)
+            #env.render('human')
+            state2, reward, done, info = env.step(action)
+            learn(state, state2, reward, action)
+            state = state2
+            t += 1
+            crewards+=reward
+            if done:
+                print(episode)
+                break
+        if decay_rate != None:
+            epsilon = initial_epsilon + (final_epsilon - initial_epsilon) * np.exp(-decay_rate * episode) 
+        epsilon = initial_epsilon + (final_epsilon - initial_epsilon) * (episode+1)/total_episodes
+        erewards.append(crewards);
+        #print(episode, epsilon)
+      # os.system('clear')
+            #time.sleep(0.1)
+    #print ("Score over time: ", rewards/total_episodes)
+    #print ("Score in last trial: ", erewards)
+    return (Q, erewards)
+    #with open("frozenLake_qTable_sarsa.pkl", 'wb') as f:
+        #pickle.dump(Q, f)
