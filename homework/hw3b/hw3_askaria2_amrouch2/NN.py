cd021b5652882019818a1a980e6c07505ef56666
Massi0 <amr.massinissa@gmail.com>
2018-09-27 16:37:15 -0500

    TF Structure ~OK, TF basic test:Pass

--- homework/hw3b/hw3_askaria2_amrouch2/NN.py
+++ homework/hw3b/hw3_askaria2_amrouch2/NN.py
@@ -1,62 +1,108 @@
 import tensorflow as tf
 import numpy as np
 
-class DQNet:
 
-    def __init__(self):
+class FFNeuralNet(object):
 
-        self.action = 3
-        self.state = 4
-        self.epsilon = 0.1
-        self.layer1 = 30
-        self.trainer = tf.train.AdamOptimizer()
 
-        self.input_observation = tf.placeholder(shape= (self.state, 1) , dtype= tf.float32)
+    def __init__(self,input_size,layer1,output_size):
+        #Define a FeedFroward network that take state as input and outputs the the Qfunction that correspend to each action i.e Q(a_i) = o_i, for i=1,2,3
 
+        self.input_observation = tf.placeholder(shape= [input_size, 1] , dtype= tf.float32)
+        self.input_action = input_action = tf.placeholder(shape= [output_size, 1] , dtype= tf.float32)
+        
         # now declare the weights connecting the input to the hidden layer
-        self.W1 = tf.Variable(tf.random_normal([self.state, self.layer1], stddev=0.03), name='W1')
-        self.b1 = tf.Variable(tf.random_normal([self.layer1, 1]), name='b1')
+        self.W1 = tf.Variable(tf.random_normal([input_size, layer1], stddev=0.03), name='W1')
+        self.b1 = tf.Variable(tf.random_normal([layer1, 1]), name='b1')
+        
         # and the weights connecting the hidden layer to the output layer
-        self.W2 = tf.Variable(tf.random_normal([self.layer1, self.action], stddev=0.03), name='W2')
-        self.b2 = tf.Variable(tf.random_normal([self.action, 1]), name='b2')
+        self.W2 = tf.Variable(tf.random_normal([layer1, output_size], stddev=0.03), name='W2')
+        self.b2 = tf.Variable(tf.random_normal([output_size, 1]), name='b2')
 
         # calculate the output of the hidden layer
-        self.hidden_out = tf.add(tf.matmul(tf.transpose(self.W1), self.input_observation), self.b1)
-        self.hidden_out = tf.nn.relu(self.hidden_out)
+        self.hidden_out = tf.nn.relu(tf.add(tf.matmul(tf.transpose(self.W1), self.input_observation), self.b1))
 
         # now calculate the hidden layer output - in this case, let's use a softmax activated
         # output layer
         self.predict = tf.nn.softmax(tf.add(tf.matmul(tf.transpose(self.W2), self.hidden_out), self.b2))
+        #returns the Q value that correspend to the the index(input_action) != 0
+        self.Q = tf.reduce_sum (tf.multiply(self.input_action,self.predict))
 
-        self.init = tf.global_variables_initializer()
-        self.init_l = tf.local_variables_initializer()
-        tf.Session().run(self.init)
-        tf.Session().run(self.init_l)
-
-    def BackProp(self, Qnext):
         
-        loss = tf.reduce_sum(tf.square(Qnext-self.predict))
-        updateModel = self.trainer.minimize(loss)
-        tf.Session().run(updateModel)
+    def get_Qvalue(self):
+            #return pointers to the input_actions and Q value
+        return self.Q, self.input_action 
 
-    def ActSel(self, s, greed):
+    def get_Output_NN(self):
+        return self.predict
+    def get_Input(self):
+        return self.input_observation
+        
+
+
+class DQNet:
+
+    def __init__(self):
+
+        self.action = output_size = 3
+        self.state = input_size = 6
+        self.epsilon = 0.1
+        self.layer1 = layer1 = 30
+
+        self.trainer = tf.train.AdamOptimizer()
+
+        self.input_size = self.state 
+        self.sess = sess =  tf.Session()
+
+        #Action-Value Qfunc
+        self.Q = Q = FFNeuralNet(input_size,layer1,output_size)
+        self.initQ =  initQ = tf.trainable_variables()
+
+        #Target Action-Value Qfunc
+        self.Q_pred = Q_pred = FFNeuralNet(input_size,layer1,output_size)
+        
+        self.initQp = initQp = tf.trainable_variables()[len(initQ):]
+
+        sess.run(tf.global_variables_initializer())
+        #Initialize Qp = Q
+        sess.run([initQp[i].assign(initQ[i]) for i in range(len(initQ))]) 
+
+        
+        self.y_target = y_target = tf.placeholder(shape= [1] , dtype= tf.float32)
+        Qvalue,_ = Q.get_Qvalue()
+        
+        self.loss = loss = tf.reduce_sum(tf.square(y_target-Qvalue))
+        self.optim = self.trainer.minimize(loss)
+        self.maxActionQ = tf.argmax(Q.get_Output_NN(),axis = 0)
+
+        
+    def updateModel(self):
+        return self.optim
+
+
+    def Session(self):
+        return self.sess
+
+    def ActionSelection(self, s,random_sample_action, greedy = 1):
 
         # implements a greedy method for selection of action
+        #greedy = 0 for greedy 1 otherewise
         
-        if greed >= 0.5:
+        if greedy >= 0.5:
             greed_sel = np.random.random_sample()
         else:
             greed_sel = 1
 
-        predict = tf.Session().run(self.predict, feed_dict={self.input_observation: s})
+        #predict = self.sess.run(self.Q.get_Output_NN(),)
 
 
         if greed_sel >= self.epsilon:
-            predict_max = tf.argmax(predict,0)
+            predicted_Action = self.sess.run(self.maxActionQ,feed_dict= {self.Q.get_Input():np.reshape(s,[self.state,1])})[0]
+            
         else:
-            predict_max = np.random.randint(low = 0, high = self.action)
+            predicted_Actionx =  random_sample_action()
 
-        return predict_max, predict
+        return predicted_Action
         
 
 
--- homework/hw3b/hw3_askaria2_amrouch2/main.py
+++ homework/hw3b/hw3_askaria2_amrouch2/main.py
@@ -1,4 +1,4 @@
-from acrobot import AcrobotEnv
+from acrobot import AcroBotMEnv as AcrobotEnv
 from time import sleep
 from NN import DQNet
 import numpy as np
@@ -6,34 +6,68 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 
 
-env = AcrobotEnv()
-learn = DQNet()
+class Memory_Replay_Wrapper(object):
+    def __init__(self,max_len=1e4):
+        self.scenes = []
+        self.max_len = max_len
 
-episode_num = 1
-time_horizon = 1000
 
-for i_episode in range(episode_num):
-    observation = env.reset()
+    def push_scene(self,s_0,a_0,r_0,s_1):
+        wrap = {'s0':s_0,
+                'a0':a_0,
+                'r0':r_0,
+                's1':s_1}
+
+        self.scenes.append(wrap)
+        if len(self.scenes)> self.max_len:
+            rem self.scenes[0]
+
+    def pop_scene(self,idx):
+        if idx>self.max_len || idx<0:
+            raise ValueError('Wrong Index in scenes')
+
+        scene = self.scenes[idx]
+
+        s0 = scene['s0']
+        a0 = scene['a0']    
+        r0 = scene['r0']
+        s1 = scene['s1']
+
+        return s0,a0,r0,s1
+
+    
+if __name__ == '__main__':
+
+    env = AcrobotEnv()
+    learn = DQNet()
+
+    episode_num = 1
+    time_horizon = 1000
     init = tf.global_variables_initializer()
-    init_l = tf.local_variables_initializer()
-    tf.Session().run(init)
-    tf.Session().run(init_l)
-    for t in range(time_horizon):
-        env.render()
+    sess = learn.Session()
+    sess.run(init)
 
-        # using greedy algorithm to select action
-        action, target_Q = learn.ActSel(observation,1)
+    for i_episode in range(episode_num):
+        observation = env.reset()
 
-        observation, reward, term, x = env.step(action)
-
-        action_old = action
-        
-        # action selection without greedy
-        action, Q_val = learn.ActSel(observation, 0)
-
-        target_Q[action_old] = reward + 0.95*np.max(Q_val)
-        
-        learn.BackProp(target_Q)
-        
-    env.close()
+        for t in range(time_horizon):
+            env.render()
+            
+            # using greedy algorithm to select action
+            new_action = learn.ActionSelection(observation, env.action_space.sample, 1 )
+            
+            observation, reward = env.step(new_action)
+            
+            action_old = action
+            
+            # action selection without greedy
+            action, Q_val = learn.ActSel(observation, 0, sess.run)
+            if t+1 == time_horizon:
+                target_Q[action_old] = reward
+            else:
+                target_Q[action_old] = reward + 0.95*np.max(Q_val)
+            
+            sess.run(learn.BackProp(target_Q))
+            
+            env.close()
 
