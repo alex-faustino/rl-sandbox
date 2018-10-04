9bfa37bbed60478fd57d3aa716596a3aa7882a65
VedantFNO <123ved@gmail.com>
2018-10-04 04:41:52 -0500

    Still some bugs

--- homework/hw4/hw4_vedant2/Agents/discreteAgent.py
+++ homework/hw4/hw4_vedant2/Agents/discreteAgent.py
@@ -8,7 +8,7 @@ Created on Wed Sep 12 22:03:38 2018
 import time, pickle, os
 import numpy as np
 
-def SARSA(env,initial_epsilon = 1, final_epsilon = 0.01,total_episodes = 1000, annealing_period = None,max_steps = 10,lr_rate = 0.99,gamma = 0.9, decay_rate = None):
+def SARSA(env,initial_epsilon = 1, final_epsilon = 0.01,total_episodes = 5000, annealing_period = None,max_steps = 25,lr_rate = 0.99,gamma = 0.9, decay_rate = None):
     if (annealing_period == None):
         annealing_period = total_episodes;
     if(annealing_period > total_episodes):
@@ -61,7 +61,7 @@ def SARSA(env,initial_epsilon = 1, final_epsilon = 0.01,total_episodes = 1000, a
     #with open("frozenLake_qTable_sarsa.pkl", 'wb') as f:
         #pickle.dump(Q, f)
         
-def QLearn(env,initial_epsilon = 1, final_epsilon = 0.01,total_episodes = 1000, annealing_period = None,max_steps = 10,lr_rate = 0.99,gamma = 0.9, decay_rate = None):
+def QLearn(env,initial_epsilon = 1, final_epsilon = 0.01,total_episodes = 5000, annealing_period = None,max_steps = 25,lr_rate = 0.99,gamma = 0.9, decay_rate = None):
     if (annealing_period == None):
         annealing_period = total_episodes;
     if(annealing_period > total_episodes):
@@ -112,7 +112,7 @@ def QLearn(env,initial_epsilon = 1, final_epsilon = 0.01,total_episodes = 1000, 
     #with open("frozenLake_qTable_sarsa.pkl", 'wb') as f:
         #pickle.dump(Q, f)
         
-def Reinforce(env,initial_epsilon = 1, final_epsilon = 0.01,total_episodes = 1000, annealing_period = None,max_steps = 10,lr_rate = 0.09, gamma = 1,decay_rate = None):
+def Reinforce(env,initial_epsilon = 1, final_epsilon = 0.01,total_episodes = 5000, annealing_period = None,max_steps = 25,lr_rate = 0.9, gamma = 1,decay_rate = None):
     if (annealing_period == None):
         annealing_period = total_episodes;
     if(annealing_period > total_episodes):
@@ -150,17 +150,18 @@ def Reinforce(env,initial_epsilon = 1, final_epsilon = 0.01,total_episodes = 100
         update = np.zeros((env.observation_space.n, env.action_space.n))
     
         G = np.zeros(len(rewards))
-        G[-1] = rewards[-1]
-        for i in range(2, len(G) + 1):
-            G[-i] =  gamma*G[-i + 1] + rewards[-i]
+        #G[-1] = rewards[-1]
+        for i in range(2, len(G)):
+            G[i] =  G[i-1] + rewards[i]
         
         for i in range(len(G)):
             action = np.zeros((env.action_space.n))
-            action[actions[i]] = 1 
+            action[actions[i]] = 1.0 
             pmf = P[states[i]]
             grad_ln_pi = action - pmf
-            update[states[i]] += lr_rate * G[i] * grad_ln_pi
-        Q = P + (update)
+            update[states[i]] += G[i] * grad_ln_pi
+            update = update /max_steps
+        Q =(update) - lr_rate*((update) - P)
         
         return apply_softmax(Q)
             #theta_list[states[i]] = theta_list[states[i]]/np.linalg.norm(theta_list[states[i]])
@@ -170,7 +171,7 @@ def Reinforce(env,initial_epsilon = 1, final_epsilon = 0.01,total_episodes = 100
         action_list = []
         state_list = []
         reward_list = []
-        crewards=0
+        crewards=0.0
         t = 0
         state = env.reset()
         state_list.append(state)
