d2c41bf03f313ccfd0a7de144234396c4b0004d5
barelySentient <allanaxelrodufl@gmail.com>
2018-10-04 09:40:23 -0500

    I have encountered a problem when trying to separate agent from gridworld

--- homework/hw3a/hw3a_allana2_catching_up/Gridworld_Env.py
+++ homework/hw3a/hw3a_allana2_catching_up/Gridworld_Env.py
@@ -69,21 +69,24 @@ class gridworld(gym.Env):
     self.previous_previous_y = self.previous_y
     self.update_q_label = 0
     self.episode_counter += 1
-    pass
-  def step(self):
+    return self.location_x, self.location_y, self.previous_x, self.previous_y, self.previous_previous_x, self.previous_previous_y, self.update_q_label, self.episode_counter
+  def step(self, my_reward, desired_action, location_x, location_y, previous_x, previous_y, previous_previous_x, previous_previous_y):
     self.previous_previous_x = self.previous_x
     self.previous_previous_y = self.previous_y
     self.previous_x = self.location_x# only accurate for second step in episode
     self.previous_y = self.location_y# only accurate for second step in episode
-    desired_action = self.action
+#    desired_action = self.action
     if np.random.rand() <= 0.1: # this part of the method is to enforce a 10% chance of a random transition
-        self.action = self.allowed_actions[0,np.random.randint(1,self.allowed_actions.shape[1])]
+        self.action = self.allowed_actions[0,np.random.randint(0,self.allowed_actions.shape[1])]#think the 1 should be changed to a 0 for the first argument of randint
     if self.my_reward[self.location_y,self.location_x] > 0:
         self.location_x = 3+1
         self.location_y = 2+1
-        if self.my_reward[self.previous_y,self.previous_x] > 5:
-            self.location_x = 1+1
-            self.location_y = 0+1
+    elif self.my_reward[self.location_y,self.location_x] > 5:
+        self.location_x = 1+1
+        self.location_y = 0+1
+#    elif self.my_reward[self.location_y,self.location_x] < 0:
+#        self.location_y = self.previous_y
+#        self.location_x = self.previous_x
     elif self.action == 1: # this part of the method is to select the desired deterministic action
         self.location_y += 1
     elif self.action == 2:
@@ -93,52 +96,54 @@ class gridworld(gym.Env):
     elif self.action == 4:
         self.location_x += 1
     self.action = desired_action# reports selected action, but has the agent move according to transition probability
-    pass #return [(self.location_x, self.location_y)]
-world = gridworld()
-if world.render_label == 't':
-    fig, (ax) = world.render_init()
-k=0 # counter for episodic cumulative reward
-for i in range(1,world.episode_length * world.num_episodes - 1):
+    return (self.location_x, self.location_y, self.previous_x, self.previous_y, self.previous_previous_x, self.previous_previous_y)
+  def work(self):
+   if world.render_label == 't':
+       fig, (ax) = world.render_init()
+   k=0 # counter for episodic cumulative reward
+   for i in range(1,world.episode_length * world.num_episodes - 1):
 #    world.update_reward_model()
-    world.my_reward_log[0,i] = world.my_reward[world.location_y,world.location_x]
-    world.my_state_log[:,i] = np.array([world.location_x,world.location_y])[np.newaxis]
+       world.my_reward_log[0,i] = world.my_reward[world.location_y,world.location_x]
+       world.my_state_log[:,i] = np.array([world.location_x,world.location_y])[np.newaxis]
 #    world.update_my_q_function()#update is for previous state, so put before state reversion
-    world.update_q_label += 1# default is to update the q function after the first iteration
-    if world.my_reward[world.location_y,world.location_x] < 0:
-        world.location_x = world.previous_x
-        world.location_y = world.previous_y
+       world.update_q_label += 1# default is to update the q function after the first iteration
+       if world.my_reward[world.location_y,world.location_x] < 0:
+           world.location_x = world.previous_x
+           world.location_y = world.previous_y
 #    world.my_policy() # closest to pragmatic results here
-    world.step()# current state is now state AFTER action has been taken
-    if np.mod(i+1,world.episode_length) == 0:
-        world.my_episodic_cumulative_reward_log[0,k] = \
-        np.sum(world.my_reward_log[0,(k*world.episode_length):(i+1)])# sums from k*episode_length to i
-        k += 1
-        world.reset()
-    progress_checker = np.floor(0.1*world.episode_length*world.num_episodes)
-    if np.mod(i+1,progress_checker) == 0:
-        sys.stdout.write("\r"+"%s" % int(10+np.floor(i/progress_checker)*10) + '%')#updates progress without excessive output
-    if world.render_label == 't':
-        world.render(fig,ax,i)
-sys.stdout.write("\r"+'done' + '\n')#displays complete progress and prints results on new lines
-fig1, (ax1)=plt.subplots()
-ax1.plot(world.my_episodic_cumulative_reward_log[0,0:-1])
-plt.xlabel('episode number')
-plt.ylabel('total episodic reward')
-print('reward model' + '\n' + str(np.flipud(world.my_reward_model[1:6,1:6])))
-print('Q function for up action' + '\n' + str(np.flipud(np.transpose(world.my_q_function[1:6,1:6,0]))))
-print('Q function for down action' + '\n' + str(np.flipud(np.transpose(world.my_q_function[1:6,1:6,1]))))
-print('Q function for left action' + '\n' + str(np.flipud(np.transpose(world.my_q_function[1:6,1:6,2]))))
-print('Q function for right action' + '\n' + str(np.flipud(np.transpose(world.my_q_function[1:6,1:6,3]))))
-print('total reward = ' + str(np.sum(world.my_reward_log[0,-world.episode_length:-1])))
-fig2, (ax2) = plt.subplots()
-ax2.plot(world.my_reward_log[0,-world.episode_length:-1])
-plt.xlabel('time step of episode')
-plt.ylabel('reward')
-fig3, (ax3) = plt.subplots()
-ax3.plot(np.transpose(world.my_state_log[0,-world.episode_length:-1]), label='x coordinate')
-ax3.plot(np.transpose(world.my_state_log[1,-world.episode_length:-1]), label='y coordinate')
-plt.xlabel('time step of episode')
-plt.ylabel('x and y coordinates')
-pylab.legend(loc='upper left')
-print('Exploit policy of agent, where: 1 is up, 2 is down, 3 is left and 4 is right')
-print(np.flipud(world.my_exploit_action_log[1:6,1:6]).astype(int))
+       world.step()# current state is now state AFTER action has been taken
+       if np.mod(i+1,world.episode_length) == 0:
+           world.my_episodic_cumulative_reward_log[0,k] = \
+           np.sum(world.my_reward_log[0,(k*world.episode_length):(i+1)])# sums from k*episode_length to i
+           k += 1
+           world.reset()
+       progress_checker = np.floor(0.1*world.episode_length*world.num_episodes)
+       if np.mod(i+1,progress_checker) == 0:
+           sys.stdout.write("\r"+"%s" % int(10+np.floor(i/progress_checker)*10) + '%')#updates progress without excessive output
+       if world.render_label == 't':
+           world.render(fig,ax,i)
+   sys.stdout.write("\r"+'done' + '\n')#displays complete progress and prints results on new lines
+   fig1, (ax1)=plt.subplots()
+   ax1.plot(world.my_episodic_cumulative_reward_log[0,0:-1])
+   plt.xlabel('episode number')
+   plt.ylabel('total episodic reward')
+   print('reward model' + '\n' + str(np.flipud(world.my_reward_model[1:6,1:6])))
+   print('Q function for up action' + '\n' + str(np.flipud(np.transpose(world.my_q_function[1:6,1:6,0]))))
+   print('Q function for down action' + '\n' + str(np.flipud(np.transpose(world.my_q_function[1:6,1:6,1]))))
+   print('Q function for left action' + '\n' + str(np.flipud(np.transpose(world.my_q_function[1:6,1:6,2]))))
+   print('Q function for right action' + '\n' + str(np.flipud(np.transpose(world.my_q_function[1:6,1:6,3]))))
+   print('total reward = ' + str(np.sum(world.my_reward_log[0,-world.episode_length:-1])))
+   fig2, (ax2) = plt.subplots()
+   ax2.plot(world.my_reward_log[0,-world.episode_length:-1])
+   plt.xlabel('time step of episode')
+   plt.ylabel('reward')
+   fig3, (ax3) = plt.subplots()
+   ax3.plot(np.transpose(world.my_state_log[0,-world.episode_length:-1]), label='x coordinate')
+   ax3.plot(np.transpose(world.my_state_log[1,-world.episode_length:-1]), label='y coordinate')
+   plt.xlabel('time step of episode')
+   plt.ylabel('x and y coordinates')
+   pylab.legend(loc='upper left')
+   print('Exploit policy of agent, where: 1 is up, 2 is down, 3 is left and 4 is right')
+   print(np.flipud(world.my_exploit_action_log[1:6,1:6]).astype(int))
+   pass
+#world = gridworld()
--- homework/hw3a/hw3a_allana2_catching_up/Q_Learning_Agent.py
+++ homework/hw3a/hw3a_allana2_catching_up/Q_Learning_Agent.py
@@ -8,7 +8,8 @@ from gym.utils import seeding
 import matplotlib.ticker as plticker
 
 class qLearning(object):
-    def __init__(self): 
+    def __init__(self, env): 
+        self.env = env
         self.render_label = 'f'
         self.gridnum = int(7) #size of gridagent
         self.location_x = np.random.randint(int(1),self.gridnum-1)
@@ -49,7 +50,7 @@ class qLearning(object):
         self.my_exploit_action_log[self.location_y,self.location_x] = int(self.action)
         if np.random.rand() <= self.my_epsilon:
             self.action = self.allowed_actions[0,np.random.randint(0,self.allowed_actions.shape[1])]
-        return self.location_y,self.location_x,self.action
+        pass# self.location_y,self.location_x,self.action
     def update_reward_model(self):# reversed y and x for reward (not model) to accommodate human-readable reward
         self.my_reward_model[self.location_y,self.location_x] = self.my_reward[self.location_y,self.location_x]
         pass
@@ -60,51 +61,58 @@ class qLearning(object):
             self.my_gamma*np.amax(self.my_q_function[self.previous_x,self.previous_y,:])-\
             self.my_q_function[self.previous_previous_x,self.previous_previous_y,self.previous_action-1])
         pass
-agent = qLearning()
-if agent.render_label == 't':
-        fig, (ax) = agent.render_init()
-k=0 # counter for episodic cumulative reward
-for i in range(1,agent.episode_length * agent.num_episodes - 1):
-    agent.update_reward_model()
-    agent.my_reward_log[0,i] = agent.my_reward[agent.location_y,agent.location_x]
-    agent.my_state_log[:,i] = np.array([agent.location_x,agent.location_y])[np.newaxis]
-    agent.update_my_q_function()#update is for previous state, so put before state reversion
-    agent.update_q_label += 1# default is to update the q function after the first iteration
-    if agent.my_reward[agent.location_y,agent.location_x] < 0:
-        agent.location_x = agent.previous_x
-        agent.location_y = agent.previous_y
-    agent.my_policy() # closest to pragmatic results here
-#    agent.step()# current state is now state AFTER action has been taken
-    if np.mod(i+1,agent.episode_length) == 0:
-        agent.my_episodic_cumulative_reward_log[0,k] = \
-        np.sum(agent.my_reward_log[0,(k*agent.episode_length):(i+1)])# sums from k*episode_length to i
-        k += 1
-#        agent.reset()
-    progress_checker = np.floor(0.1*agent.episode_length*agent.num_episodes)
-    if np.mod(i+1,progress_checker) == 0:
-        sys.stdout.write("\r"+"%s" % int(10+np.floor(i/progress_checker)*10) + '%')#updates progress without excessive output
-    if agent.render_label == 't':
-        agent.render(fig,ax,i)
-sys.stdout.write("\r"+'done' + '\n')#displays complete progress and prints results on new lines
-fig1, (ax1)=plt.subplots()
-ax1.plot(agent.my_episodic_cumulative_reward_log[0,0:-1])
-plt.xlabel('episode number')
-plt.ylabel('total episodic reward')
-print('reward model' + '\n' + str(np.flipud(agent.my_reward_model[1:6,1:6])))
-print('Q function for up action' + '\n' + str(np.flipud(np.transpose(agent.my_q_function[1:6,1:6,0]))))
-print('Q function for down action' + '\n' + str(np.flipud(np.transpose(agent.my_q_function[1:6,1:6,1]))))
-print('Q function for left action' + '\n' + str(np.flipud(np.transpose(agent.my_q_function[1:6,1:6,2]))))
-print('Q function for right action' + '\n' + str(np.flipud(np.transpose(agent.my_q_function[1:6,1:6,3]))))
-print('total reward = ' + str(np.sum(agent.my_reward_log[0,-agent.episode_length:-1])))
-fig2, (ax2) = plt.subplots()
-ax2.plot(agent.my_reward_log[0,-agent.episode_length:-1])
-plt.xlabel('time step of episode')
-plt.ylabel('reward')
-fig3, (ax3) = plt.subplots()
-ax3.plot(np.transpose(agent.my_state_log[0,-agent.episode_length:-1]), label='x coordinate')
-ax3.plot(np.transpose(agent.my_state_log[1,-agent.episode_length:-1]), label='y coordinate')
-plt.xlabel('time step of episode')
-plt.ylabel('x and y coordinates')
-pylab.legend(loc='upper left')
-print('Exploit policy of agent, where: 1 is up, 2 is down, 3 is left and 4 is right')
-print(np.flipud(agent.my_exploit_action_log[1:6,1:6]).astype(int))
+    def work(self):
+        if self.render_label == 't':
+            fig, (ax) = self.render_init()
+        k=0 # counter for episodic cumulative reward
+        for i in range(1,self.episode_length * self.num_episodes - 1):
+            self.update_reward_model()
+            self.my_reward_log[0,i] = self.my_reward[self.location_y,self.location_x]
+            self.my_state_log[:,i] = np.array([self.location_x,self.location_y])[np.newaxis]
+            self.update_my_q_function()#update is for previous state, so put before state reversion
+            self.update_q_label += 1# default is to update the q function after the first iteration
+            if self.my_reward[self.location_y,self.location_x] < 0:
+              print('episode',k,'time step',i)
+              print('current then previous',self.location_x,self.location_y,self.previous_x,self.previous_y)
+              self.location_y = self.previous_y
+              self.location_x = self.previous_x
+              print('current',self.location_x,self.location_y)
+            self.my_policy() # closest to pragmatic results here
+            (location_x, location_y, previous_x, previous_y, previous_previous_x, previous_previous_y) = self.env.step(self.my_reward, self.action, self.location_x, self.location_y, self.previous_x, self.previous_y, self.previous_previous_x, self.previous_previous_y)# current state is AFTER action
+            (self.location_x, self.location_y, self.previous_x, self.previous_y, self.previous_previous_x, self.previous_previous_y) = (location_x, location_y, previous_x, previous_y, previous_previous_x, previous_previous_y)
+            print('after policy current',self.location_x,self.location_y)
+            if np.mod(i+1,self.episode_length) == 0:
+                self.my_episodic_cumulative_reward_log[0,k] = \
+                np.sum(self.my_reward_log[0,(k*self.episode_length):(i+1)])# sums from k*episode_length to i
+                k += 1
+                self.env.reset()
+            progress_checker = np.floor(0.1*self.episode_length*self.num_episodes)
+            if np.mod(i+1,progress_checker) == 0:
+                sys.stdout.write("\r"+"%s" % int(10+np.floor(i/progress_checker)*10) + '%')#updates progress without excessive output
+            if self.render_label == 't':
+                self.render(fig,ax,i)
+        sys.stdout.write("\r"+'done' + '\n')#displays complete progress and prints results on new lines
+        fig1, (ax1)=plt.subplots()
+        ax1.plot(self.my_episodic_cumulative_reward_log[0,0:-1])
+        plt.xlabel('episode number')
+        plt.ylabel('total episodic reward')
+        print('reward model' + '\n' + str(np.flipud(self.my_reward_model[1:6,1:6])))
+        print('Q function for up action' + '\n' + str(np.flipud(np.transpose(self.my_q_function[1:6,1:6,0]))))
+        print('Q function for down action' + '\n' + str(np.flipud(np.transpose(self.my_q_function[1:6,1:6,1]))))
+        print('Q function for left action' + '\n' + str(np.flipud(np.transpose(self.my_q_function[1:6,1:6,2]))))
+        print('Q function for right action' + '\n' + str(np.flipud(np.transpose(self.my_q_function[1:6,1:6,3]))))
+        print('total reward = ' + str(np.sum(self.my_reward_log[0,-self.episode_length:-1])))
+        fig2, (ax2) = plt.subplots()
+        ax2.plot(self.my_reward_log[0,-self.episode_length:-1])
+        plt.xlabel('time step of episode')
+        plt.ylabel('reward')
+        fig3, (ax3) = plt.subplots()
+        ax3.plot(np.transpose(self.my_state_log[0,-self.episode_length:-1]), label='x coordinate')
+        ax3.plot(np.transpose(self.my_state_log[1,-self.episode_length:-1]), label='y coordinate')
+        plt.xlabel('time step of episode')
+        plt.ylabel('x and y coordinates')
+        pylab.legend(loc='upper left')
+        print('Exploit policy of agent, where: 1 is up, 2 is down, 3 is left and 4 is right')
+        print(np.flipud(self.my_exploit_action_log[1:6,1:6]).astype(int))
+        pass 
+#agent = qLearning(env)
