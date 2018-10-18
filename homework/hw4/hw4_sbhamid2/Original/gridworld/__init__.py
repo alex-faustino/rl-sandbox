301bb50ddc1dd022669a3acc9ab475d1e46f24a5
sriramyab <sriramyab@gmail.com>
2018-10-18 09:09:01 -0500

    HW submission

--- homework/hw4/hw4_sbhamid2/Original/gridworld/__init__.py
+++
@@ -1,8 +0,0 @@
-from gym.envs.registration import register
-
-register(
-    id='MyGridworld-v2',
-    entry_point='gridworld.gridworld:GridWorldEnvNew',
-    max_episode_steps=500,
-    reward_threshold=475.0,
-)
