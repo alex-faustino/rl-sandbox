1784e4a4c9ff23fa87b4d0a7d9b67e66da727250
Abdul Alkurdi <42843403+AbdulAlkurdi@users.noreply.github.com>
2018-10-19 19:23:35 -0500

    Add files via upload

--- homework/hw2/hw2_alkurdi2/Qlearn.py
+++ homework/hw2/hw2_alkurdi2/Qlearn.py
@@ -2,54 +2,41 @@
 """
 Abdul Alkurdi
 AE598
-Qlearning
+Qlearning Algorithm
 """
-
 from random import random
 import numpy as np
-
-class Qlearn(object):
-    def __init__(self,s,):
-        self.env=env
-        self.reset()
-    
-    def reset(self)
-        self.Q=np.zeros((self.env.observation_space.n,self.env.action_space.n))
-    def train(self, epsilon, alpha, gamma, epsilon_max, episode_length,epis_num):
-        self.reset()
-        rewards=np.zeros(epis.num)
-        for i in range(epis.num):
-            s=self.env.reset()
-            a=self.action_epsilon_G(s,epsilon)
-            for j in range(episode_length):
-                (s_new,r,done)=self.env.step(a)
-                rewards[i] += r
-                a_new=self.action_epsilon_g(s_new,epsilon)
-                self.Q[s,a] += alpha*(r+gamma*self.Q[s_new,a_new] - self.Q[s,a])
-                (s,a)=(s_new,a_new)
-                if done:
-                    break
-        return rewards
-    def action_greedy(self, s):
-        return self.Q[s,:].argmax()
-    def action_random(self, s):
-        return random.randrange(self.eng.action_space.n)
-    def action_epsilon(self, s,epsilon):
-        if random()<epsilon:
-            return self.action_random(s)
-        return self.action_greedy(s)
-    def enjoy(self, episode_max_length):
-        reward=np.zeros(episode_length)
-        states=np.zeros(episode_length,dtype=int)
-        actions=np.zeros(episode_length,dtype=int)
-        s=self.env.reset()
-        for j in range(episode_length):
-            a=self.action_greedy(s)
-            states[j]=s
-            actions[j]=a
-            (s_new,r,done)=self.env.step(a)
-            rewards[j]=r;
-            s=s_new
-            if done:
-                break
-        return (rewards,states,actions)
+import gym
+def Qlagent(meta, env, hard=False): 
+    [epsilon,alpha,gamma,ep_len,ep_num]= meta    
+    s_size=0
+    a_size=0
+    if type(env.action_space)==gym.spaces.discrete.Discrete:
+        a_size=env.action_space.n
+    elif type(env.action_space)==gym.spaces.multi_discrete.MultiDiscrete:
+        a_size=np.prod(env.action_space.nvec)
+    if type(env.observation_space)==gym.spaces.discrete.Discrete:
+        s_size=env.observation_space.n
+    elif type(env.observation_space)==gym.spaces.multi_discrete.MultiDiscrete:
+        s_size=np.prod(env.observation_space.nvec)
+    Q=np.zeros([a_size,s_size])
+    reward=np.zeros(ep_num)
+    for i in range(ep_num):
+        a_store=[]
+        s_store=[]
+        s=env.reset()
+        s_t=s[0]+s[1]*5
+        for j in range(ep_len):
+            s_t=s[0]+5*s[1] #one hot encoded state
+            s_store.append(s_t)
+            if random() > epsilon:
+                a=np.argmax(Q[:,s_t])
+            else:
+                a=env.action_space.sample()
+            a_store.append(a)
+            ns, r, _ , _ =env.step(a,hard)
+            ns_t=ns[0]+5*ns[1]
+            reward[i] += r
+            Q[a,s_t] += alpha*(r+ gamma* Q[np.argmax(Q[:,ns_t]),ns_t]-Q[a,s_t])
+            s=ns
+    return Q,  reward, a_store, s_store
