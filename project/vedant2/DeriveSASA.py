4cf1dce83e73a245dab121dffd860a14e3353b6b
UOFI\vedant2 <vedant2@illinois.edu>
2018-11-12 19:14:59 -0600

    Dynamic Model for Satellite implemented in SymPy

--- project/vedant2/DeriveSASA.py
+++ project/vedant2/DeriveSASA.py
@@ -31,48 +31,81 @@ Body_COM = Point('Body_COM')
 Body_COM.set_pos(O,Body_position[0]*W.x+Body_position[1]*W.y+Body_position[2]*W.z)
 Body_COM.set_vel(W,Body_vel[0]*W.x+Body_vel[1]*W.y+Body_vel[2]*W.z)
 
-Body_connection = Point('Body_connection')
-Body_connection.set_pos(Body_COM,2*Body_frame.x+3*Body_frame.y+4*Body_frame.z)
-Body_connection.set_vel(Body_frame,0)
-#Body_frame.set_ang_vel(Body_frame, 0*Body_frame.x+0*Body_frame.y+0*Body_frame.z)
-KDE_B = []
+Body_connectionR = Point('Body_connectionR')
+Body_connectionR.set_pos(Body_COM,2*Body_frame.x+3*Body_frame.y+4*Body_frame.z)
+Body_connectionR.set_vel(Body_frame,0)
+Body_connectionR.v2pt_theory(Body_COM,W,Body_frame)
 
-Body_frame.ang_vel_in(W)
+Body_connectionL = Point('Body_connectionL')
+Body_connectionL.set_pos(Body_COM,-2*Body_frame.x-3*Body_frame.y-4*Body_frame.z)
+Body_connectionL.set_vel(Body_frame,0)
+Body_connectionL.v2pt_theory(Body_COM,W,Body_frame)
+#Body_frame.set_ang_vel(Body_frame, 0*Body_frame.x+0*Body_frame.y+0*Body_frame.z)
+KDE_Body = []
+
+#Body_frame.ang_vel_in(W)
 #Body_COM.v2pt_theory(O,W,Body_frame)
 # missing linear vel!
 
-Body_connection.v2pt_theory(Body_COM,W,Body_frame)
 
-#I1, I2, I3, M_base = symbols('I1 I2 I3 M_Base')
-#B_Frame_I = (inertia(B, I1, I2, I3, 0, 0,0))
-#Base = RigidBody('Base', Body_COM, W, M_base, (B_Frame_I, Body_COM))
+
+I1_Body, I2_Body, I3_Body, M_Body = symbols('I1_Body I2_Body I3_Body M_Body')
+Body_I = (inertia(Body_frame, I1_Body, I2_Body, I3_Body, 0, 0,0))
+Body = RigidBody('Body', Body_COM, Body_frame, M_Body, (Body_I, Body_COM))
 '''
 First Right Panel Frame
 '''
 PanelR1_theta = dynamicsymbols('PR1_th:3')
-Panel_omega = dynamicsymbols('PR1_om:3')
+PanelR1_omega = dynamicsymbols('PR1_om:3')
 
 PanelR1_frame = W.orientnew('PR1', 'Body', PanelR1_theta , '123')
-PanelR1_frame.set_ang_vel(Body_frame, Panel_omega[0]*Body_frame.x+Panel_omega[1]*Body_frame.y+Panel_omega[2]*Body_frame.z)
+PanelR1_frame.set_ang_vel(Body_frame, PanelR1_omega[0]*Body_frame.x+PanelR1_omega[1]*Body_frame.y+PanelR1_omega[2]*Body_frame.z)
 #PanelR1_frame.ang_vel_in(W)
 '''
 PanelR1_connection = Point('PR1_O')
-PanelR1_connection.set_pos(Body_connection,0*Body_frame.x+0*Body_frame.y+0*Body_frame.z)# Body_frame or PanelR1_frame?
+PanelR1_connection.set_pos(Body_connectionR,0*Body_frame.x+0*Body_frame.y+0*Body_frame.z)# Body_frame or PanelR1_frame?
 PanelR1_connection.set_vel(PanelR1_frame,0*Body_frame.x+0*Body_frame.y+0*Body_frame.z)
-PanelR1_connection.v2pt_theory(Body_connection,Body_frame,PanelR1_frame)
+PanelR1_connection.v2pt_theory(Body_connectionR,Body_frame,PanelR1_frame)
 '''
 PanelR1_COM = Point('PR1_COM')
-PanelR1_COM.set_pos(Body_connection,1*PanelR1_frame.x+2*PanelR1_frame.y+3*PanelR1_frame.z)
+PanelR1_COM.set_pos(Body_connectionR,1*PanelR1_frame.x+2*PanelR1_frame.y+3*PanelR1_frame.z)
 #PanelR1_COM.set_vel(PanelR1_frame,0)
-PanelR1_COM.v2pt_theory(Body_connection,Body_frame,PanelR1_frame)
+PanelR1_COM.v2pt_theory(Body_connectionR,Body_frame,PanelR1_frame)
 
-KDE_R1 = [PanelR1_theta[0].diff() - Panel_omega[0],
-       PanelR1_theta[1].diff() - Panel_omega[1],
-       PanelR1_theta[2].diff() - Panel_omega[2]]
+KDE_PanelR1 = [PanelR1_theta[0].diff() - PanelR1_omega[0],
+       PanelR1_theta[1].diff() - PanelR1_omega[1],
+       PanelR1_theta[2].diff() - PanelR1_omega[2]]
 
+I1_PanelR1, I2_PanelR1, I3_PanelR1, M_PanelR1 = symbols('I1_PanelR1 I2_PanelR1 I3_PanelR1 M_PanelR1')
+PanelR1_I = (inertia(PanelR1_frame, I1_PanelR1, I2_PanelR1, I3_PanelR1, 0, 0, 0))
+PanelR1 = RigidBody('PanelR1', PanelR1_COM, PanelR1_frame, M_PanelR1, (PanelR1_I, PanelR1_COM))
+'''
+First Left Panel Frame
+'''
+PanelL1_theta = dynamicsymbols('PL1_th:3')
+PanelL1_omega = dynamicsymbols('PL1_om:3')
 
+PanelL1_frame = W.orientnew('PL1', 'Body', PanelL1_theta , '123')
+PanelL1_frame.set_ang_vel(Body_frame, PanelL1_omega[0]*Body_frame.x+PanelL1_omega[1]*Body_frame.y+PanelL1_omega[2]*Body_frame.z)
+#PanelR1_frame.ang_vel_in(W)
+'''
+PanelR1_connection = Point('PR1_O')
+PanelR1_connection.set_pos(Body_connectionR,0*Body_frame.x+0*Body_frame.y+0*Body_frame.z)# Body_frame or PanelR1_frame?
+PanelR1_connection.set_vel(PanelR1_frame,0*Body_frame.x+0*Body_frame.y+0*Body_frame.z)
+PanelR1_connection.v2pt_theory(Body_connectionR,Body_frame,PanelR1_frame)
+'''
+PanelL1_COM = Point('PL1_COM')
+PanelL1_COM.set_pos(Body_connectionL,1*PanelL1_frame.x+2*PanelL1_frame.y+3*PanelL1_frame.z)
+#PanelR1_COM.set_vel(PanelR1_frame,0)
+PanelL1_COM.v2pt_theory(Body_connectionL,Body_frame,PanelL1_frame)
 
+KDE_PanelL1 = [PanelL1_theta[0].diff() - PanelL1_omega[0],
+       PanelL1_theta[1].diff() - PanelL1_omega[1],
+       PanelL1_theta[2].diff() - PanelL1_omega[2]]
 
+I1_PanelL1, I2_PanelL1, I3_PanelL1, M_PanelL1 = symbols('I1_PanelL1 I2_PanelL1 I3_PanelL1 M_PanelL1')
+PanelL1_I = (inertia(PanelL1_frame, I1_PanelL1, I2_PanelL1, I3_PanelL1, 0, 0, 0))
+PanelL1 = RigidBody('PanelL1', PanelL1_COM, PanelL1_frame, M_PanelL1, (PanelL1_I, PanelL1_COM))
 '''
 from sympy import symbols
 import sympy.physics.mechanics as me
