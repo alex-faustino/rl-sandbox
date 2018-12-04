71434bff0f42e9fd68c8c4fdbbc6fc400b8fe934
Andre Gou <andrejgou@gmail.com>
2018-12-04 00:31:04 -0600

    attempt for data generation

---
+++ project/agou2_dusad2/code/dataset.py
@@ -0,0 +1,31 @@
+#%%
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+from imageio import imread
+import os
+from constants import DATASET_SIZE, HEIGHT, WIDTH, ROLLOUT_DIR
+
+#%%
+class VAEDataset(Dataset):
+    def __init__(self, rollout_dir=ROLLOUT_DIR, size=DATASET_SIZE, transform=None):
+        self.transform=transform
+        self.frames = np.zeros((size, HEIGHT, WIDTH, 3), dtype=int)
+        episodes = [ep for ep in os.listdir(ROLLOUT_DIR) if '.npz' in ep]
+        num_frames = 0
+        for episode in episodes:
+            ep_frames = np.load(os.path.join(ROLLOUT_DIR, episode))['frames']
+            self.frames[num_frames:num_frames + len(ep_frames)] = ep_frames
+            num_frames = min(num_frames + len(ep_frames), size)
+            if num_frames == size:
+                break
+
+    def __len__(self):
+        return len(self.frames)
+    
+    def __getitem__(self, idx):
+        sample = self.frames[idx]
+        if self.transform:
+            sample = self.transform(sample)
+        return sample
+
---
+++ project/agou2_dusad2/code/generate_data.py
@@ -0,0 +1,36 @@
+#%%
+import numpy as np
+import gym
+import random
+from cv2 import resize
+import os
+from constants import ROLLOUT_DIR, NUM_EPISODES, NUM_STEPS
+
+if not os.path.exists(ROLLOUT_DIR):
+    os.makedirs(ROLLOUT_DIR)
+
+
+env = gym.make('MountainCar-v0')
+
+for ep in range(NUM_EPISODES):
+    random_int = random.randint(0, 2**31-1)
+    frames = []
+    actions = []
+    state = env.reset()
+    for step in range(NUM_STEPS):
+        im = env.render(mode='rgb_array')
+        im = resize(im, (64,64))
+        frames.append(im)
+
+        action = env.action_space.sample()
+        actions.append(action)
+
+        env.step(action)
+    frames = np.array(frames, dtype=int)
+    actions = np.array(actions, dtype=float)
+    filename = os.path.join(ROLLOUT_DIR, str(random_int)+".npz")
+    np.savez_compressed(filename, frames=frames, actions=actions)
+
+env.close()
+
+
