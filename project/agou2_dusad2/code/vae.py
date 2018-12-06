75ea9a7bd747bf8b6c0b2138c76c5b6b2e5e122d
Andre Gou <andrejgou@gmail.com>
2018-12-06 00:03:04 -0600

    fixed error in kl loss

--- project/agou2_dusad2/code/vae.py
+++ project/agou2_dusad2/code/vae.py
@@ -73,7 +73,7 @@ class VAE(nn.Module):
         return torch.mean(torch.sum((output_imgs - input_imgs)**2, (1, 2, 3)))
 
     def kl_loss(self, logvar, mu):
-        kl_loss = -0.5 + torch.sum(1 + logvar - mu**2 - torch.exp(logvar), 1)
+        kl_loss = -0.5 * torch.sum(1 + logvar - mu**2 - torch.exp(logvar), 1)
         kl_loss = torch.max(kl_loss, torch.FloatTensor([self.kl_tolerance*self.z_size]).expand_as(kl_loss))
         kl_loss = torch.mean(kl_loss)
         return kl_loss  
