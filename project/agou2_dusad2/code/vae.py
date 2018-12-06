9dee93122e1e0c70f7485e53fec7effa0a3e8915
Ubuntu <ubuntu@ip-172-31-17-79.ec2.internal>
2018-12-06 06:26:55 +0000

    cuda changes + notebook

--- project/agou2_dusad2/code/vae.py
+++ project/agou2_dusad2/code/vae.py
@@ -17,13 +17,13 @@ class Reshape(nn.Module):
 
 
 class VAE(nn.Module):
-    def __init__(self, z_size=LATENT_SIZE, conv_sizes=[4, 8, 16, 32], batch_size=1, learning_rate=0.0001, kl_tolerance=0.5, gpu=False):
+    def __init__(self, z_size=LATENT_SIZE, conv_sizes=[4, 8, 16, 32], batch_size=1, learning_rate=0.0001, kl_tolerance=0.5, device=None):
         self.z_size=z_size
         self.conv_sizes = conv_sizes # hardcoding to be 4 deep
         self.batch_size=batch_size
         self.learning_rate=learning_rate
         self.kl_tolerance=kl_tolerance
-        self.gpu = gpu
+        self.device = device
         
         super(VAE, self).__init__()
         
@@ -36,13 +36,13 @@ class VAE(nn.Module):
             nn.ReLU(),
             nn.Conv2d(self.conv_sizes[2], self.conv_sizes[3], kernel_size=4, stride=2),
             nn.ReLU(),
-        )
+        ).to(self.device)
         
-        self.mu = nn.Linear(self.conv_sizes[3]*2*2, self.z_size)
-        self.logvar = nn.Linear(self.conv_sizes[3]*2*2, self.z_size)
+        self.mu = nn.Linear(self.conv_sizes[1]*5*5, self.z_size).to(self.device)
+        self.logvar = nn.Linear(self.conv_sizes[1]*5*5, self.z_size).to(self.device)
         
         self.decoder = nn.Sequential(
-            nn.Linear(self.z_size,4*self.conv_sizes[3]),
+            nn.Linear(self.z_size,4*self.conv_sizes[1]),
             Reshape((-1,  4*self.conv_sizes[3], 1, 1)),
             nn.ConvTranspose2d(4*self.conv_sizes[3], self.conv_sizes[2], kernel_size=5, stride=2),
             nn.ReLU(),
@@ -52,7 +52,7 @@ class VAE(nn.Module):
             nn.ReLU(),
             nn.ConvTranspose2d(self.conv_sizes[0], 3, kernel_size=6, stride=2),
             nn.Sigmoid()
-        )
+        ).to(self.device)
         
         
     def forward(self, original):
@@ -62,7 +62,7 @@ class VAE(nn.Module):
         mu = self.mu(encoded)
         logvar = self.logvar(encoded)
         sigma = torch.exp(logvar/2.0)
-        eps = torch.randn(self.batch_size, self.z_size)
+        eps = torch.randn(self.batch_size, self.z_size).cuda(device=self.device)
         z = mu + sigma * eps
         
         decoded = self.decoder(z)
@@ -77,7 +77,16 @@ class VAE(nn.Module):
         kl_loss = torch.max(kl_loss, torch.FloatTensor([self.kl_tolerance*self.z_size]).expand_as(kl_loss))
         kl_loss = torch.mean(kl_loss)
         return kl_loss  
+
+#         kl_loss = -0.5*torch.sum(1 + logvar - mu**2 - torch.exp(logvar), 1)
+#         kl_loss = torch.max(kl_loss, torch.FloatTensor([self.kl_tolerance*self.z_size]).expand_as(kl_loss).cuda(self.device))
+#         kl_loss = torch.mean(kl_loss)
+#         KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+#         return KLD  
+
     
     def loss(self, original, decoded, mu, logvar):
-        return self.kl_loss(logvar, mu) + self.reconstruction_loss(original, decoded)
+        recon = self.reconstruction_loss(original, decoded)
+        kl = self.kl_loss(logvar, mu)
+        return  kl + recon, kl , recon
     
